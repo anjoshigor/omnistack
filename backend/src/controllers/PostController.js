b20c64522db6e301144be5b1c011a949b60eaa79
Higor Anjos <higor.anjos@conductor.com.br>
2019-06-13 18:02:18 -0300

    Backend finished :heavy_check_mark:

--- backend/src/controllers/PostController.js
+++ backend/src/controllers/PostController.js
@@ -1,4 +1,7 @@
 const Post = require("../models/Post");
+const sharp = require("sharp");
+const path = require("path");
+const fs = require("fs");
 
 module.exports = {
   async index(req, res) {
@@ -9,6 +12,16 @@ module.exports = {
   async store(req, res) {
     const { author, place, description, hashtags } = req.body;
     const { filename: image } = req.file;
+    const [name] = image.split(".");
+
+    const filename = `${name}.jpg`;
+
+    await sharp(req.file.path)
+      .resize(500)
+      .jpeg({ quality: 70 })
+      .toFile(path.resolve(req.file.destination, "resized", filename));
+
+    fs.unlinkSync(req.file.path);
 
     const post = await Post.create({
       author,
@@ -17,6 +30,9 @@ module.exports = {
       hashtags,
       image
     });
-    res.json(post);
+
+    req.io.emit("post", post);
+
+    return res.json(post);
   }
 };
